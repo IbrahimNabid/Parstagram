cb70ea37ef6f7106505a5088c5797c385f53a7dd
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2022-01-01 23:05:52 -0500

    Added infinite scroll and refresh features

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Parse
 import AlamofireImage
+var numberOfPosts: Int!
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -19,8 +20,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         let post = posts[indexPath.row]
         let user = post["author"] as! PFUser
         
-        cell.usernameLabel.text = user.username
-        cell.captionLabel.text = post["caption"] as? String
+        cell.usernameLabel.text = ( user.username)
+        cell.captionLabel.text = (post["caption"] as! String)
         
         let imageFile = post["image"] as! PFFileObject
         let urlString = imageFile.url!
@@ -34,30 +35,61 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
 @IBOutlet var tableView: UITableView!
 var posts = [PFObject]()
-    
+
+let myRefreshControl = UIRefreshControl()
 override func viewDidLoad() {
     super.viewDidLoad()
     
     tableView.delegate = self
     tableView.dataSource = self
     
+    myRefreshControl.addTarget(self, action: #selector(viewDidAppear), for: .valueChanged)
+    tableView.refreshControl = myRefreshControl
+    
 
 // Do any additional setup after loading the view.
 }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let query = PFQuery(className: "Posts")
-        query.includeKey("author")
-        query.limit = 20
+
+override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    numberOfPosts = 20
+    let query = PFQuery(className: "Posts")
+    query.includeKey("author")
+    query.limit = numberOfPosts
         
-        query.findObjectsInBackground { (posts, error) in
-            if posts != nil
-            {
-                self.posts = posts!
-                self.tableView.reloadData()
-            }
+    query.findObjectsInBackground { (posts, error) in
+        if posts != nil
+        {
+            self.posts = posts!
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
         }
     }
+}
+    
+func loadMorePosts(){
+    numberOfPosts = numberOfPosts + 5
+    let query = PFQuery(className: "Posts")
+    query.includeKey("author")
+    query.limit = numberOfPosts
+        
+    query.findObjectsInBackground { (posts, error) in
+        if posts != nil
+        {
+            self.posts = posts!
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        }
+    }
+}
+
+func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    if indexPath.row + 1 == posts.count {
+        loadMorePosts()
+    }
+}
+    
+
     
 
     /*
