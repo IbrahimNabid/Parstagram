c290e56dc069f2a365e422c52b34e4f1e693b718
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2022-01-11 19:04:14 -0500

    Added comment feature

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  CommentCell
+//
+//  Created by Ibrahim Nabid on 1/6/22.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
